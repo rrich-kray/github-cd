fa89bcfcb33f6d833b92d1363ee1f99ee120bb0e
Ross Ritchey <ross.ritchey@gmail.com>
2022-02-10 21:12:06 -0500

    Add in-class query examples

---
+++ 12-SQL/01-Activities/04-SQL-Join/Solved/db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO prices (price)
+VALUES (1.50),
+       (2.75),
+       (5.50),
+       (10.25),
+       (15.75),
+	   (19.95),
+	   (21.53);
+
+INSERT INTO books (book_name, price)
+VALUES ("The Great Gatsby", 1),
+       ("Huckleberry Finn", 3),
+       ("100 Years of Solitude", 5),
+       ("Things Fall Apart", 1),
+       ("Crime and Punishment", 2),
+       ("Moby Dick",  4),
+       ("Decameron", 1),
+	   ("Harry Potter", NULL);
